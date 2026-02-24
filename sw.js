20c8bf2b5d7ac393428e28d356142aa78e99e206
muhonne <hmmuhonen@gmail.com>
2026-02-24 22:17:40 +0200

    wcag compliance and larger interactable elements

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'tikkaus-v1';
-const ASSETS = ['./', 'index.html', 'manifest.json', 'icon.svg'];
+const CACHE = 'tikkaus-v2';
+const ASSETS = ['./', 'index.html', 'manifest.json', 'icon.svg', 'image.png'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
